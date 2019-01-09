7597f63a3e66ea3e12b10d8b11d42834905e6b81
Marek Sautter <marek.sautter@gmail.com>
2019-01-09 08:49:25 -0800

    Added Ch1 and Ch2
    Added file structures and visual studio projects to each folder for the BAMF team

---
+++ Cracking the Coding Interview/Chapter 1  - Arrays/1 - Allison/1 - Exercises/1 - Exercises/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#include "Ex 1.1.h"
+#include "Ex 1.2.h"
+#include "Ex 1.3.h"
+#include "Ex 1.4.h"
+#include "Ex 1.5.h"
+#include "Ex 1.6.h"
+#include "Ex 1.7.h"
+#include "Ex 1.8.h"
+#include "Ex 1.9.h"
+
+
+int main()
+{
+
+	system("PAUSE");
+	return 0;
+}
---
+++ Cracking the Coding Interview/Chapter 2  - Linked Lists/2 - Allison/2 - Exercises/2 - Exercises/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+#include "Ex 2.1.h"
+#include "Ex 2.2.h"
+#include "Ex 2.3.h"
+#include "Ex 2.4.h"
+#include "Ex 2.5.h"
+#include "Ex 2.6.h"
+#include "Ex 2.7.h"
+#include "Ex 2.8.h"
+
+using namespace std;
+
+int main()
+{
+	system("PAUSE");
+	return 0;
+}
